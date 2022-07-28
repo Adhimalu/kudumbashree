cd606f61ac5eb1906d27085f87c51b10e40a6b81
Adhimalu <adhimalu678@gmail.com>
2022-07-28 08:32:33 +0530

    kudumbashree completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { KudumbashreeComponent } from './kudumbashree/kudumbashree.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    KudumbashreeComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/kudumbashree/kudumbashree.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-kudumbashree',
+  templateUrl: './kudumbashree.component.html',
+  styleUrls: ['./kudumbashree.component.css']
+})
+export class KudumbashreeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
